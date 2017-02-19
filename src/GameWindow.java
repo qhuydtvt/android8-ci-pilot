4c0ff76a14a5f332f4f2364389f42bfc40fcf0e3
huynq <qhuydtvt@gmail.com>
2017-02-19 22:29:11 +0700

    Session1: Added plane and moving right

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,11 +1,21 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by huynq on 2/19/17.
  */
 public class GameWindow extends Frame {
+    Image backgroundImage;
+    Image planeImage;
+    private int planeX = (400 - 35) / 2;
+
     public GameWindow() {
         setVisible(true);
         setSize(400, 600);
@@ -24,5 +34,51 @@ public class GameWindow extends Frame {
                 System.out.printf("windowClosed");
             }
         });
+
+        // 1: Load image
+        backgroundImage = loadImageFromRes("background.png");
+        planeImage = loadImageFromRes("plane3.png");
+
+        // 2: Draw image
+        repaint();
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    // TODO: move plane to right
+                    planeX += 10;
+                    repaint();
+                    System.out.println("keyPressed");
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                super.keyReleased(e);
+            }
+        });
+    }
+
+    private Image loadImageFromRes(String url) {
+        try {
+            Image image = ImageIO.read(new File("resources/" + url));
+            return image;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void update(Graphics g) {
+        g.drawImage(backgroundImage, 0, 0, 400, 600, null);
+        g.drawImage(planeImage, planeX, 600 - 25, 35, 25, null);
     }
 }
